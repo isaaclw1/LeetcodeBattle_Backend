9e2207ef55e8080300a8e7c1367b77aae095aa45
Isaac Wang <111948990+isaaclw1@users.noreply.github.com>
2024-04-21 18:24:43 -0700

    created the springboot backend

---
+++ leetcodebattle/src/main/java/com/springboot/leetcodebattle/LeetcodebattleApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.leetcodebattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodebattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodebattleApplication.class, args);
+	}
+
+}
---
+++ leetcodebattle/src/test/java/com/springboot/leetcodebattle/LeetcodebattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.leetcodebattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodebattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
